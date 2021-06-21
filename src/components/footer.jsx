3dd9fdccfa4baaa365518873000f093db4288409
phartenfeller <pharti@hotmail.de>
2021-06-21 11:57:24 +0200

    added f1 dsicalimer in the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -29,6 +29,12 @@ const Footer = () => (
         Privacy Policy
       </a>
     </div>
+    <div className="mt-2">
+      This website is unofficial and is not associated in any way with the
+      Formula 1 companies. F1, FORMULA ONE, FORMULA 1, FIA FORMULA ONE WORLD
+      CHAMPIONSHIP, GRAND PRIX and related marks are trade marks of Formula One
+      Licensing B.V.
+    </div>
   </footer>
 );
 
