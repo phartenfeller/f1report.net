c3c01dde7a71e0dadb77680474dd6e6010927c00
phartenfeller <pharti@hotmail.de>
2021-10-24 16:03:10 +0200

    more table scorllbars

--- src/components/race-template/raceResultsTable.jsx
+++ src/components/race-template/raceResultsTable.jsx
@@ -6,85 +6,89 @@ import TeamDisplay from '../teamDisplay/teamDisplay';
 const RaceResultsTable = ({ resultsByRaceidList }) => (
   <div className="py-2 align-middle min-w-full">
     <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th scope="col" className="c-table-heading">
-              #
-            </th>
-            <th scope="col" className="c-table-heading">
-              Driver
-            </th>
-            <th scope="col" className="c-table-heading">
-              Constructor
-            </th>
-            <th scope="col" className="c-table-heading">
-              Pts.
-            </th>
-            <th scope="col" className="c-table-heading">
-              Time
-            </th>
-            <th scope="col" className="c-table-heading">
-              Result
-            </th>
-            <th scope="col" className="c-table-heading">
-              Fastest L.
-            </th>
-            <th scope="col" className="c-table-heading">
-              Grid
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {resultsByRaceidList.map(
-            (
-              {
-                resultid,
-                fastestlap,
-                constructorTeamByConstructorid,
-                driverByDriverid,
-                grid,
-                laps,
-                points,
-                position,
-                statusByStatusid,
-                time: finishTime,
-                fastestlapspeed,
-                fastestlaptime,
-              },
-              i
-            ) => (
-              <tr
-                key={resultid}
-                className={i % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
-              >
-                <td className="c-table-cell font-medium">{position}</td>
-                <td className="c-table-cell">
-                  {driverByDriverid.forename} {driverByDriverid.surname}{' '}
-                  {driverByDriverid.number
-                    ? `(${driverByDriverid.number})`
-                    : null}
-                </td>
-                <td className="c-table-cell">
-                  <TeamDisplay teamName={constructorTeamByConstructorid.name} />
-                </td>
-                <td className="c-table-cell">{points}</td>
-                <td className="c-table-cell">{finishTime}</td>
-                <td className="c-table-cell" title={`Laps: ${laps}`}>
-                  {statusByStatusid.status}
-                </td>
-                <td
-                  className="c-table-cell"
-                  title={`Lap: ${fastestlap}, Speed: ${fastestlapspeed}`}
+      <div className="overflow-x-auto overflow-hidden">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th scope="col" className="c-table-heading">
+                #
+              </th>
+              <th scope="col" className="c-table-heading">
+                Driver
+              </th>
+              <th scope="col" className="c-table-heading">
+                Constructor
+              </th>
+              <th scope="col" className="c-table-heading">
+                Pts.
+              </th>
+              <th scope="col" className="c-table-heading">
+                Time
+              </th>
+              <th scope="col" className="c-table-heading">
+                Result
+              </th>
+              <th scope="col" className="c-table-heading">
+                Fastest L.
+              </th>
+              <th scope="col" className="c-table-heading">
+                Grid
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {resultsByRaceidList.map(
+              (
+                {
+                  resultid,
+                  fastestlap,
+                  constructorTeamByConstructorid,
+                  driverByDriverid,
+                  grid,
+                  laps,
+                  points,
+                  position,
+                  statusByStatusid,
+                  time: finishTime,
+                  fastestlapspeed,
+                  fastestlaptime,
+                },
+                i
+              ) => (
+                <tr
+                  key={resultid}
+                  className={i % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
                 >
-                  {fastestlaptime}
-                </td>
-                <td className="c-table-cell">{grid}</td>
-              </tr>
-            )
-          )}
-        </tbody>
-      </table>
+                  <td className="c-table-cell font-medium">{position}</td>
+                  <td className="c-table-cell">
+                    {driverByDriverid.forename} {driverByDriverid.surname}{' '}
+                    {driverByDriverid.number
+                      ? `(${driverByDriverid.number})`
+                      : null}
+                  </td>
+                  <td className="c-table-cell">
+                    <TeamDisplay
+                      teamName={constructorTeamByConstructorid.name}
+                    />
+                  </td>
+                  <td className="c-table-cell">{points}</td>
+                  <td className="c-table-cell">{finishTime}</td>
+                  <td className="c-table-cell" title={`Laps: ${laps}`}>
+                    {statusByStatusid.status}
+                  </td>
+                  <td
+                    className="c-table-cell"
+                    title={`Lap: ${fastestlap}, Speed: ${fastestlapspeed}`}
+                  >
+                    {fastestlaptime}
+                  </td>
+                  <td className="c-table-cell">{grid}</td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 );
--- src/components/season-template/driverStandingTable.jsx
+++ src/components/season-template/driverStandingTable.jsx
@@ -9,54 +9,56 @@ const DriverStandingsTable = ({
 }) => (
   <div className="py-2 align-middle inline-block min-w-full">
     <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th scope="col" className="c-table-heading">
-              #
-            </th>
-            <th scope="col" className="c-table-heading">
-              Driver
-            </th>
-            <th scope="col" className="hidden md:c-table-heading">
-              Team
-            </th>
-            <th scope="col" className="hidden lg:c-table-heading">
-              Wins
-            </th>
-            <th scope="col" className="c-table-heading">
-              Points
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {standings.map(
-            ({ position, points, wins, driverByDriverid, driverid }, i) => (
-              <tr
-                key={driverid}
-                className={i % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
-              >
-                <td className="c-table-cell font-medium">{position}</td>
-                <td className="c-table-cell font-medium">
-                  {driverByDriverid.driverDisplayName}
-                </td>
-                <td className="hidden md:c-table-cell">
-                  <TeamDisplay
-                    driverId={driverid}
-                    seasondrivermainconsByYearList={
-                      seasondrivermainconsByYearList
-                    }
-                  />
-                </td>
-                <td className="hidden lg:c-table-cell">{wins}</td>
-                <td className="c-table-cell text-right pr-4 md:text-left md:pr-0">
-                  {points}
-                </td>
-              </tr>
-            )
-          )}
-        </tbody>
-      </table>
+      <div className="overflow-x-auto overflow-hidden">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              <th scope="col" className="c-table-heading">
+                #
+              </th>
+              <th scope="col" className="c-table-heading">
+                Driver
+              </th>
+              <th scope="col" className="hidden md:c-table-heading">
+                Team
+              </th>
+              <th scope="col" className="hidden lg:c-table-heading">
+                Wins
+              </th>
+              <th scope="col" className="c-table-heading">
+                Points
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {standings.map(
+              ({ position, points, wins, driverByDriverid, driverid }, i) => (
+                <tr
+                  key={driverid}
+                  className={i % 2 === 0 ? 'bg-white' : 'bg-gray-50'}
+                >
+                  <td className="c-table-cell font-medium">{position}</td>
+                  <td className="c-table-cell font-medium">
+                    {driverByDriverid.driverDisplayName}
+                  </td>
+                  <td className="hidden md:c-table-cell">
+                    <TeamDisplay
+                      driverId={driverid}
+                      seasondrivermainconsByYearList={
+                        seasondrivermainconsByYearList
+                      }
+                    />
+                  </td>
+                  <td className="hidden lg:c-table-cell">{wins}</td>
+                  <td className="c-table-cell text-right pr-4 md:text-left md:pr-0">
+                    {points}
+                  </td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 );
