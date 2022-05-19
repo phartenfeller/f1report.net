137f35b29d703de54667ae37e2ae725a60e722f7
phartenfeller <pharti@hotmail.de>
2022-05-19 14:24:46 +0200

    remove serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
     `gatsby-plugin-postcss`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    `gatsby-plugin-remove-serviceworker`,
     // {
     //   resolve: `gatsby-plugin-advanced-sitemap`,
     //   options: {
